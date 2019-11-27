a2eecdaade293f5b2e7b9892eeaef31024a73bd9
Rafał Borowski <rafall.borowski@gmail.com>
2019-11-27 12:49:47 +0100

    add new item, form

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import './ListItem.css';
+import React from "react";
+import styles from "./ListItem.module.scss";
+import PropTypes from "prop-types";
 
-const ListItem = ({
-    name, image, description, twitterLink
-}) => (
-    <li className="listItem__wrapper">
-        <img 
-        alt={name}
-        src={image} 
-        className="listItem__image"/>
-        <div>
-        <h2 className="listItem__name">{name}</h2>
-        <p className="listItem__description">{description}</p>
-            <a href={twitterLink} className="listItem__button">visit twitter page</a>
-        </div>
-    </li>
-)
+const ListItem = ({ name, image, description, twitterLink }) => (
+  <li className={styles.wrapper}>
+    <img alt={name} src={image} className={styles.image} />
+    <div>
+      <h2 className={styles.name}>{name}</h2>
+      <p className={styles.description}>{description}</p>
+      <a
+        href={twitterLink}
+        className={styles.button}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        visit twitter page
+      </a>
+    </div>
+  </li>
+);
+
+ListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  twitterLink: PropTypes.string.isRequired
+};
+
+ListItem.defaultProps = {
+  description: "One of the React creations"
+};
 
 export default ListItem;
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
-import './ListWrapper.css';
-import { twitterAccounts } from '../../data/twitterAccounts'
+import styles from './ListWrapper.css';
 
-const ListWrapper = () => (
-    <ul className="listWrapper__wrapper">
-        {twitterAccounts.map((item) => (
+
+const ListWrapper = (props) => (
+    <ul className={styles.wrapper}>
+        {props.items.map((item) => (
         <ListItem 
             key={item.name}
             {...item}
